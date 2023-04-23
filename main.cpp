581de3c1efdcb1d37d6e82efc9da1389b0f1f765
Kelthon <kelthonbalbino@gmail.com>
2023-04-23 20:15:45 -0300

    added sum and mult matrix functions

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 
 #include "App.h"
 #include "Vertex2D.h"
+#include "Utils.h"
 
-#define APPNAME "Open Paint"
 #define APPWIDTH 800
 #define APPHEIGHT 600
+#define APPNAME "Open Paint"
 #define SELECTION_TOLERANCE 2.5
 
 using namespace std;
@@ -62,15 +63,15 @@ void mouse(int button, int state, int x, int y)
                 mousePosition[0] = x;
                 mousePosition[1] = y;
 
-                //future: Add point
-                 glBegin(GL_QUADS);
-                    glColor4f(100, 100, 100, 50);
-                    glVertex2f(x - SELECTION_TOLERANCE, y - SELECTION_TOLERANCE);
-                    glVertex2f(x + SELECTION_TOLERANCE, y - SELECTION_TOLERANCE);
-                    glVertex2f(x + SELECTION_TOLERANCE, y + SELECTION_TOLERANCE);
-                    glVertex2f(x - SELECTION_TOLERANCE, y + SELECTION_TOLERANCE);
-                glEnd();
-                glutSwapBuffers();
+//                //future: Add point
+//                 glBegin(GL_QUADS);
+//                    glColor4f(100, 100, 100, 50);
+//                    glVertex2f(x - SELECTION_TOLERANCE, y - SELECTION_TOLERANCE);
+//                    glVertex2f(x + SELECTION_TOLERANCE, y - SELECTION_TOLERANCE);
+//                    glVertex2f(x + SELECTION_TOLERANCE, y + SELECTION_TOLERANCE);
+//                    glVertex2f(x - SELECTION_TOLERANCE, y + SELECTION_TOLERANCE);
+//                glEnd();
+//                glutSwapBuffers();
             }
             break;
 
@@ -101,14 +102,62 @@ void display()
     glutSwapBuffers();
 }
 
+
+float * sum(float * M, float * N)
+{
+    float P[9];
+
+    for(int element = 0; element < 9; element++)
+    {
+        P[element] = M[element] + N[element];
+    }
+
+}
+
+float * mult(float * M, float * N)
+{
+    float P[9];
+    int i, j, k;
+    char *number, *rowNumber, *columnNumber;
+
+    for(int l = 0; l < 9; l++) P[l] = 0;
+
+    for(int row = 0; row < 3; row++)
+    {
+        for(int column = 0; column < 3; column++)
+        {
+            number = const_cast<char *>(fstring<int>("%d%d", row, column).c_str());
+            i = convertToInt(number, 2, 3);
+
+            for(int element = 0; element < 3; element++)
+            {
+                rowNumber = const_cast<char *>(fstring<int>("%d%d", row, element).c_str());
+                columnNumber = const_cast<char *>(fstring<int>("%d%d", element, column).c_str());
+
+                j = convertToInt(rowNumber, 2, 3);
+                k = convertToInt(columnNumber, 2, 3);
+
+                P[i] += M[j] * N[k];
+            }
+        }
+    }
+
+    return P;
+}
+
 int main(int argc, char** argv)
 {
-    App app = App(APPNAME, APPWIDTH, APPHEIGHT);
-    app.setMouseFunction(mouse);
-    app.setMouseMotionFunction(mouseSelection);
-    app.setDisplayFunction(display);
-    app.setKeyboardFunction(keyboard);
-    app.run(argc, argv);
+//    App app = App(APPNAME, APPWIDTH, APPHEIGHT);
+//    app.setMouseFunction(mouse);
+//    app.setMouseMotionFunction(mouseSelection);
+//    app.setDisplayFunction(display);
+//    app.setKeyboardFunction(keyboard);
+//    app.run(argc, argv);
 
+    float a[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    float b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    mult(a, b);
+    sum(a, b);
     return 0;
 }
